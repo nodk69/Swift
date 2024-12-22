478678f231a33ee0738188d8bee19cd8de07d1f2
Nadeem kidwai <igcmmt@gmail.com>
2024-12-23 01:00:30 +0530

    Normalize line endings

---
+++ src/main/java/com/swift/swift_platform/SwiftPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.swift.swift_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftPlatformApplication.class, args);
+	}
+
+}
